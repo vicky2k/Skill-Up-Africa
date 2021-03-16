b2fadc8e91dfdc39d03fd4068dc740a451e6583e
vicky2k <vicspidin@gmail.com>
2021-03-16 09:57:10 +0100

    Examples of Objects and Arrays

---
+++ ObjectsArrayExamples.js
@@ -0,0 +1,17 @@
+###### Javascript DOM Explanation
+
+DOM stands Document Object Model and this is a platform and language-neutral interface that allows programs and scripts to dybamically access and update the content, structure and style of a document. Javascript DOM also refers to the model of the document that forms the current web page. 
+With the DOM, JavaScript can access and change all the elements of an HTML document. The Javascript  DOM structure forms a Tree Like Outline that shows the hierachy of elements in a web page.
+
+HTML elements normally exist as objects which in turn have properties, methods and events and in JavaScript, elements in the DOM are targeted with query selector methods.
+
+Some DOM Targetting Methods within a Document include
+
+Document.body, Document.title, Document.url
+
+There are other older methods such as 
+getElementsByID
+getElementsByClassName
+getElementsByTagName
+
+etc.
